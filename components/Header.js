56a80d05a45b9a8f96bd65e2b00100fb523390ca
Alina Ray <a.alina.ray@gmail.com>
2019-10-17 12:59:14 +0300

    Platform.select and Platform in if checks was implemented

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,14 @@ import Colors from '../constans/colors';
 
 const Header = props => {
     return(
-        <View style={styles.header}>
+        <View
+            style={{
+                ...styles.headerBase,
+                ...Platform.select({
+                    ios: styles.headerIOS,
+                    android: styles.headerAndroid,
+                })
+            }}>
             <TitleText style={styles.title}>
                 {props.title}
             </TitleText>
@@ -15,15 +22,20 @@ const Header = props => {
 };
 
 const styles = StyleSheet.create({
-    header: {
+    headerBase: {
         width: '100%',
         height: 90,
         paddingTop: 36,
-        backgroundColor: Platform.OS === 'android' ? Colors.primary : 'white',
         alignItems: 'center',
         justifyContent: 'center',
-        borderBottomColor: Platform.OS === 'ios' ? '#ccc' : 'transparent',
-        borderBottomWidth: Platform.OS === 'ios' ? 1 : 0,
+    },
+    headerIOS: {
+        backgroundColor: 'white',
+        borderBottomColor: '#ccc' ,
+        borderBottomWidth: 1,
+    },
+    headerAndroid: {
+        backgroundColor: Colors.primary,
     },
     title: {
         color: Platform.OS === 'ios' ? Colors.primary : 'white',
--- components/MainButton.js
+++ components/MainButton.js
@@ -1,21 +1,33 @@
 import React from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity, TouchableNativeFeedback, Platform} from 'react-native';
 
 import Colors from '../constans/colors';
 
 const MainButton = props => {
+    let ButtonComponent = TouchableOpacity;
+
+    if (Platform.OS === 'android' && Platform.Version >= 21) {
+        ButtonComponent = TouchableNativeFeedback;
+    }
+
     return (
-        <TouchableOpacity onPress={props.onPress} activeOpacity={0.6}>
-            <View style={styles.button}>
-                <Text style={styles.buttonText}>
-                    {props.children}
-                </Text>
-            </View>
-        </TouchableOpacity>
+        <View style={styles.buttonContainer}>
+            <ButtonComponent onPress={props.onPress} activeOpacity={0.6}>
+                <View style={styles.button}>
+                    <Text style={styles.buttonText}>
+                        {props.children}
+                    </Text>
+                </View>
+            </ButtonComponent>
+        </View>
     )
 };
 
 const styles = StyleSheet.create({
+    buttonContainer: {
+        borderRadius: 25,
+        overflow: 'hidden',
+    },
     button: {
         backgroundColor: Colors.primary,
         paddingVertical: 12,
