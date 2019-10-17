ce8bccc221139512f8a03d708d62860917d9e17d
Alina Ray <a.alina.ray@gmail.com>
2019-10-17 12:42:17 +0300

    orientation changes were implemented

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, StyleSheet, Button, Image} from 'react-native';
+import {View, Text, StyleSheet, Button, Image, Dimensions, ScrollView} from 'react-native';
 
 import BodyText from '../components/BodyText';
 import TitleText from '../components/TitleText';
@@ -9,27 +9,29 @@ import Colors from '../constans/colors';
 
 const GameOverScreen = props => {
     return (
-        <View style={styles.screen}>
-            <TitleText>The Game is Over!</TitleText>
-            <View style={styles.imageContainer}>
-                <Image
-                    fadeDuration={1000}
-                    source={require('../assets/success.png')}
-                    // source={{uri: 'https://pbs.twimg.com/profile_images/438756755397300224/kp8rmJes_400x400.jpeg'}}
-                    style={styles.image}
-                    resizeMode="cover"
-                />
+        <ScrollView>
+            <View style={styles.screen}>
+                <TitleText>The Game is Over!</TitleText>
+                <View style={styles.imageContainer}>
+                    <Image
+                        fadeDuration={1000}
+                        source={require('../assets/success.png')}
+                        // source={{uri: 'https://pbs.twimg.com/profile_images/438756755397300224/kp8rmJes_400x400.jpeg'}}
+                        style={styles.image}
+                        resizeMode="cover"
+                    />
+                </View>
+                <View style={styles.resultContainer}>
+                    <BodyText style={styles.resultText}>
+                        Your phone needed
+                        <Text style={styles.hightlight}> {props.roundsNumber} </Text>
+                          rounds to guess number
+                        <Text style={styles.hightlight}> {props.userNumber}</Text>
+                    </BodyText>
+                </View>
+                <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
             </View>
-            <View style={styles.resultContainer}>
-                <BodyText style={styles.resultText}>
-                    Your phone needed
-                    <Text style={styles.hightlight}>{props.roundsNumber}</Text>
-                    rounds to guess number
-                    <Text style={styles.hightlight}>{props.userNumber}</Text>
-                </BodyText>
-            </View>
-            <MainButton  onPress={props.onRestart}>NEW GAME</MainButton>
-        </View>
+        </ScrollView>
     )
 };
 
@@ -38,15 +40,16 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        paddingVertical: 10
     },
     imageContainer: {
-        width: 300,
-        height: 300,
-        borderRadius: 150,
+        width: Dimensions.get('window').width * 0.7,
+        height: Dimensions.get('window').width * 0.7,
+        borderRadius: Dimensions.get('window').width * 0.7 / 2,
         borderWidth: 3,
         borderColor: 'black',
         overflow: 'hidden',
-        marginVertical: 30
+        marginVertical: Dimensions.get('window').height / 30,
     },
     image: {
         width: '100%',
@@ -54,10 +57,11 @@ const styles = StyleSheet.create({
     },
     resultContainer: {
         marginHorizontal: 30,
+        marginVertical: Dimensions.get('window').height / 60,
     },
     resultText: {
         textAlign: 'center',
-        fontSize: 20,
+        fontSize: Dimensions.get('window').height < 400 ? 16 : 20,
     },
     hightlight: {
         color: Colors.primary,
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -31,9 +31,25 @@ const GameScreen = props => {
 
     const [currentGuess, setCurrentGuess] = useState(initialGuess);
     const [pastGuesses, setPastGuesses] = useState([initialGuess.toString()]);
+    const [availableDeviceWidth, setAvailableDeviceWidth] = useState(
+        Dimensions.get('window').width
+    );
+    const [availableDeviceHeight, setAvailableDeviceHeight] = useState(
+        Dimensions.get('window').height
+    );
     const currentLow = useRef(1);
     const currentHight = useRef(100);
 
+    useEffect(() => {
+        const updateLayout = () => {
+            setAvailableDeviceWidth(Dimensions.get('window').width);
+            setAvailableDeviceHeight(Dimensions.get('window').height);
+        };
+        Dimensions.addEventListener('change', updateLayout);
+        return () => {
+            Dimensions.removeEventListener('change', updateLayout);
+        }
+    });
     useEffect(() => {
         if (currentGuess === userChoice) {
             onGameOver(pastGuesses.length);
@@ -61,8 +77,32 @@ const GameScreen = props => {
 
     let listContainerStyle = styles.listContainer;
 
-    if (Dimensions.get('window').width < 350) {
+    if (availableDeviceWidth < 350) {
         listContainerStyle = styles.listContainerBig;
+    }
+
+    if (availableDeviceHeight < 500) {
+        return (
+            <View style={styles.screen}>
+                <Text style={DefaultStyles.title}>Opponent's Guess</Text>
+                <View style={styles.controls}>
+                    <MainButton onPress={nextGuessHandler.bind(this, 'lower')}>
+                        <Ionicons name="md-remove" size={24} color="white"/>
+                    </MainButton>
+                    <NumberContainer>{currentGuess}</NumberContainer>
+                    <MainButton onPress={nextGuessHandler.bind(this, 'greater')}>
+                        <Ionicons name="md-add" size={24} color="white"/>
+                    </MainButton>
+                </View>
+                <View style={listContainerStyle}>
+                    <FlatList keyExtractor={(item) => item}
+                              data={pastGuesses}
+                              renderItem={renderListItem.bind(this, pastGuesses.length)}
+                              contentContainerStyle={styles.list}
+                    />
+                </View>
+            </View>
+        )
     }
     return (
         <View style={styles.screen}>
@@ -102,6 +142,12 @@ const styles = StyleSheet.create({
         marginTop: Dimensions.get('window').height > 600 ? 20 : 10,
         width: 400,
         maxWidth: '90%'
+    },
+    controls: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        width: '80%'
     },
     listContainer: {
         flex: 1,
